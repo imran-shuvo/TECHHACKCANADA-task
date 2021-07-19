8c032e9e089cb8a6f3cbce3552595755195b4843
imran-shuvo <shuvoimran3@gmail.com>
2021-07-19 23:23:02 +0600

    final commit to deployment

--- src/components/home.js
+++ src/components/home.js
@@ -10,7 +10,7 @@ const Home=(props)=>{
         <PageTitle titleText="Home Page"  />
         <div >
            
-            <i style={{color:"blue", margin:"50px", fontSize:"100px"}} className="film icon"> <a style={{fontSize:"20px",color:"black"}}  href="/series/">Series</a></i>
+            <i style={{color:"blue", margin:"50px", fontSize:"100px"}} className="film icon"> <a style={{fontSize:"20px"}}  href="/series/">Series</a></i>
             <i style={{ margin:"50px", fontSize:"100px"}} className="film icon"> <a style={{fontSize:"20px"}}  href="/movies/">Movies</a></i>
 
            
